17abfe49d669a4870dc23e8de8f2acbdf9610705
TopJames <zhanlangqi@126.com>
2018-05-24 23:10:35 +0800

    shareChannel=true

--- motanServiceBoot/src/main/java/com/MotanServiceBoot/service/impl/Brave1.java
+++ motanServiceBoot/src/main/java/com/MotanServiceBoot/service/impl/Brave1.java
@@ -7,7 +7,7 @@ import com.weibo.api.motan.config.springsupport.annotation.MotanService;
  * Created by Administrator on 2018/5/8.
  */
 //@MotanService(group = "you_are_fucking_stupid")
-//@MotanService
+@MotanService
 public class Brave1 implements BraveEnough {
     @Override
     public String ha(String content) {
